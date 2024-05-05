0ff5576d9064a2d84d030adb5718706c2e1140ea
ViKa_618 <hovikhanh2k4@gmail.com>
2024-05-05 17:01:16 +0700

    update calender v2.0

--- TableTick/obj/Debug/net7.0-windows/TableTick.AssemblyInfo.cs
+++ TableTick/obj/Debug/net7.0-windows/TableTick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db2350089379f016e1e654c06fec8a3e8d0aef02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c7772803b733982b717b2a88a76f695d78f1b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
