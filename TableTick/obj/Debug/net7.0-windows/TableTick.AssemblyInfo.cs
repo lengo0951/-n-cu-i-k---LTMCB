1e7e7b01da37b5fed0334cc32b9bc844c29d765d
NKhoauit <khoanhat2003.work@gmail.com>
2024-05-22 17:12:14 +0700

    change some UI

--- TableTick/obj/Debug/net7.0-windows/TableTick.AssemblyInfo.cs
+++ TableTick/obj/Debug/net7.0-windows/TableTick.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ffe5cde6166aedd7e00d3acdb6c6e3bc7715ac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7df6cf8c89562b4b5889732585095a1ca0d9347")]
 [assembly: System.Reflection.AssemblyProductAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TableTick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
