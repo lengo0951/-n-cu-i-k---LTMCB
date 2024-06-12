e46b93e1e54467599220d74504cafbd28db110a1
ViKa_618 <hovikhanh2k4@gmail.com>
2024-06-12 21:47:16 +0700

    fix pomodoro

--- TableTick/Forms/Pomodoro.Designer.cs
+++ TableTick/Forms/Pomodoro.Designer.cs
@@ -42,33 +42,38 @@
             // 
             // start
             // 
+            start.BackColor = Color.Aquamarine;
             start.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            start.Location = new Point(12, 349);
+            start.Location = new Point(26, 410);
+            start.Margin = new Padding(3, 4, 3, 4);
             start.Name = "start";
-            start.Size = new Size(105, 52);
+            start.Size = new Size(105, 61);
             start.TabIndex = 1;
             start.Text = "Start";
-            start.UseVisualStyleBackColor = true;
+            start.UseVisualStyleBackColor = false;
             start.Click += startButton_Click;
             // 
             // stop
             // 
+            stop.BackColor = Color.LightCoral;
             stop.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            stop.Location = new Point(182, 349);
+            stop.Location = new Point(152, 410);
+            stop.Margin = new Padding(3, 4, 3, 4);
             stop.Name = "stop";
-            stop.Size = new Size(101, 51);
+            stop.Size = new Size(105, 61);
             stop.TabIndex = 3;
             stop.Text = "Reset";
-            stop.UseVisualStyleBackColor = true;
+            stop.UseVisualStyleBackColor = false;
             stop.Click += resetButton_Click;
             // 
             // ProgressBar
             // 
             ProgressBar.FillColor = Color.FromArgb(200, 213, 218, 223);
             ProgressBar.FillThickness = 30;
-            ProgressBar.Font = new Font("Bungee", 25.8F, FontStyle.Bold, GraphicsUnit.Point);
+            ProgressBar.Font = new Font("Microsoft Sans Serif", 25.8F, FontStyle.Bold, GraphicsUnit.Point);
             ProgressBar.ForeColor = Color.MediumSeaGreen;
-            ProgressBar.Location = new Point(61, 26);
+            ProgressBar.Location = new Point(61, 31);
+            ProgressBar.Margin = new Padding(3, 4, 3, 4);
             ProgressBar.Maximum = 1500;
             ProgressBar.Minimum = 0;
             ProgressBar.Name = "ProgressBar";
@@ -78,7 +83,7 @@
             ProgressBar.ShadowDecoration.CustomizableEdges = customizableEdges1;
             ProgressBar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
             ProgressBar.ShowText = true;
-            ProgressBar.Size = new Size(299, 299);
+            ProgressBar.Size = new Size(352, 352);
             ProgressBar.TabIndex = 29;
             ProgressBar.Text = "25:00";
             ProgressBar.TextMode = Guna.UI2.WinForms.Enums.ProgressBarTextMode.Custom;
@@ -88,7 +93,7 @@
             // 
             status.AutoSize = true;
             status.Font = new Font("Arial", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            status.Location = new Point(429, 9);
+            status.Location = new Point(429, 11);
             status.Name = "status";
             status.Size = new Size(314, 45);
             status.TabIndex = 30;
@@ -96,21 +101,24 @@
             // 
             // button1
             // 
+            button1.BackColor = SystemColors.Info;
             button1.Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(331, 349);
+            button1.Location = new Point(276, 421);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(82, 44);
+            button1.Size = new Size(84, 43);
             button1.TabIndex = 31;
             button1.Text = "Set to 5s";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // tomatoWorking
             // 
             tomatoWorking.Image = Properties.Resources.giphy;
-            tomatoWorking.Location = new Point(429, 57);
+            tomatoWorking.Location = new Point(429, 67);
+            tomatoWorking.Margin = new Padding(3, 4, 3, 4);
             tomatoWorking.Name = "tomatoWorking";
-            tomatoWorking.Size = new Size(344, 307);
+            tomatoWorking.Size = new Size(344, 361);
             tomatoWorking.SizeMode = PictureBoxSizeMode.Zoom;
             tomatoWorking.TabIndex = 32;
             tomatoWorking.TabStop = false;
@@ -118,18 +126,19 @@
             // tomatoBreaking
             // 
             tomatoBreaking.Image = Properties.Resources.tomatobreak;
-            tomatoBreaking.Location = new Point(429, 57);
+            tomatoBreaking.Location = new Point(429, 67);
+            tomatoBreaking.Margin = new Padding(3, 4, 3, 4);
             tomatoBreaking.Name = "tomatoBreaking";
-            tomatoBreaking.Size = new Size(344, 307);
+            tomatoBreaking.Size = new Size(344, 361);
             tomatoBreaking.SizeMode = PictureBoxSizeMode.Zoom;
             tomatoBreaking.TabIndex = 33;
             tomatoBreaking.TabStop = false;
             // 
             // Pomodoro
             // 
-            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(794, 478);
+            ClientSize = new Size(794, 562);
             Controls.Add(tomatoBreaking);
             Controls.Add(tomatoWorking);
             Controls.Add(button1);
@@ -137,6 +146,7 @@
             Controls.Add(ProgressBar);
             Controls.Add(stop);
             Controls.Add(start);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Pomodoro";
             Text = "Pomodoro";
             ((System.ComponentModel.ISupportInitialize)tomatoWorking).EndInit();
